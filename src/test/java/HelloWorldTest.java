d40a2775921dddaba9b76b5e33a1fe3cc63d1df0
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-09-20 22:29:24 +0300

    Параметры запроса.
    Параметры запроса.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,24 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
 import org.junit.jupiter.api.Test;
 
-public class HelloWorldTest {
+import java.util.HashMap;
+import java.util.Map;
 
+
+public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
+    public void testRestAssured(){
+
+
+        Map<String, String> params= new HashMap<>();
+        params.put("name", "John");
+        
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
-        System.out.println(response.asString());
+        response.prettyPrint();
     }
 }
