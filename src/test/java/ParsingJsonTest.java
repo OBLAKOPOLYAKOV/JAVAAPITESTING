7889e75552cc6d25a8f47a81aed5a7fbe4bb3f4a
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-09-21 08:21:25 +0300

    Ex5: Парсинг JSON
    В рамках этой задачи нужно создать тест, который будет делать GET-запрос на адрес https://playground.learnqa.ru/api/get_json_homework
    
    Полученный JSON необходимо распечатать и изучить. Мы увидим, что это данные с сообщениями и временем, когда они были написаны. Наша задача вывести текст второго сообщения.
    
    Ответом должна быть ссылка на тест в вашем репозитории.

---
+++ src/test/java/ParsingJsonTest.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ParsingJsonTest {
+    @Test
+    public void testParsingJson(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        
+
+        String message2 = response.get("messages[1].message");
+        System.out.println(message2);
+
+
+    }
+}
