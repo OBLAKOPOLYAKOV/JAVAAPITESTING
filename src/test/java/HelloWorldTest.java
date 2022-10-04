45b9ba37971dbdfcd76f731c04be5833ff03e3a7
OBLAKOPOLYAKOV <misha.polyakov.96@gmail.com>
2022-10-04 16:36:25 +0300

    02. Параметризованный тестГиперссылка

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,6 +3,8 @@ import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,21 +14,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class HelloWorldTest {
-    @Test
-    public void testFor200(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/map")
-                .andReturn();
-        assertEquals(200, response.statusCode(), "Unexpected status code");
-
-    }
-
-    @Test
-    public void testFor404(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/map2")
-                .andReturn();
-        assertEquals(404, response.statusCode(), "Unexpected status code");
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John", "Pete"})
+    public void testHelloMethodWithoutName(String name){
+        Map<String, String> queryParams = new HashMap<>();
+        if (name.length()>0){
+            queryParams.put("name", name);
+        }
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedName = (name.length()>0)? name: "someone";
+        assertEquals("Hello, "+ expectedName, answer,"The answer is not expected");
 
     }
 }
